5a9a88ba50aba0083dec11a461ee702ad805bcf6
Daniel Quispe <danielqamaro@gmail.com>
2024-08-23 17:30:34 -0500

    Este es un envio de prueba

---
+++ sumar.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int num1 = 5;
+    int num2 = 10;
+    int suma = num1 + num2;
+
+    std::cout << "Suma total: " << suma << std::endl;
+
+    return 0;
+}
